ca904d129d6a3ee185d1fe59da370cc01c7a1bce
rikky <son.levuthai@gmail.com>
2019-07-04 01:46:16 +0700

    cs fixes

--- src/Database/DatabaseServiceProvider.js
+++ src/Database/DatabaseServiceProvider.js
@@ -7,22 +7,19 @@ export default class DatabaseServiceProvider extends ServiceProvider {
 
     register() {
         this.container.singleton(Database, container => {
-            const config = container.make(Config);
-
-            const {defaultConnection, connections} = config.get('database');
+            const config     = container.make(Config);
 
             const KnexDriver = connectionName => {
 
-                const connectionConfig = connections[connectionName];
+                const connectionConfig = config.get(`database.connections.${connectionName}`);
 
-                if (!connectionConfig) {
-                    throw new Error(`E_DATABASE: Connection ${connectionName} is not configured`);
-                }
+                if (!connectionConfig) throw new Error(`E_DATABASE: Connection ${connectionName} is not configured`);
 
                 return knex(connectionConfig);
             };
 
-            return new DatabaseManager(defaultConnection).registerDriver('knex', KnexDriver);
+            return new DatabaseManager(config.get('database.defaultConnection')).registerDriver('knex', KnexDriver);
         });
     }
+
 }
