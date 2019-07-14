9f3fa5b5f458e8268544ccd28241657a3e0cfca0
rikky <son.levuthai@gmail.com>
2019-07-15 03:09:20 +0700

    adding framework as sub command

--- src/Console/index.js
+++ src/Console/index.js
@@ -1,4 +1,4 @@
-export const command = ['framework'];
+export const command = ['framework', '$0'];
 export const desc    = "Fusion CLI";
 export const builder = yargs => {
     yargs.commandDir(__dirname + '/commands')
